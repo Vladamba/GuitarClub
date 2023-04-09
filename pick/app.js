aee42b008b17a1168b40105623c62741e3a1044f
Vladamba <mvladislav777@gmail.com>
2023-04-09 22:56:47 +0300

    header, footer and home is beautiful

--- pick/app.js
+++ pick/app.js
@@ -1,14 +1,14 @@
 let questions = [{
-    question: "Boobs or ass?",
-    answers: ["boobs", "ass", "both", "dicks"],
+    question: "If your string break, you will..?",
+    answers: ["Change this string", "Change all strings", "Break other strings and change them", "Play without using the broken string"],
 },
 {
-    question: "Bass or poebalu?",
-    answers: ["boobs", "ass", "bass", "i am dumb"],
+    question: "What type of girls/boys do you like?",
+    answers: ["Blond with blue eyes", "Warm soul", "Has a lot of money", "Don't give a damn"],
 },
 {
-    question: "% or $?",
-    answers: ["#", "%", "both", "$"],
+    question: "What do you do in your free time?",
+    answers: ["Play my guitar", "Play my 8 string guitar", "Play my bass", "Sleep"],
 }];
 
 let currentQuestion = 0;
@@ -18,11 +18,15 @@ let buttons = new Array(questions[0].answers.length);
 function start(){
     currentQuestion = 0;
     points = 0;
+    document.getElementById("mainButton").classList.add("hide");
     for (let i = 0; i < buttons.length; i++){
         buttons[i] = document.createElement("button");
+        buttons[i].classList.add("button");
         buttons[i].onclick = function(){endQuestion(i)};
         document.getElementById("answersContainer").append(buttons[i]);
     }
+    document.getElementById("image").src = "";
+    document.getElementById("image").classList.remove("active");
     startQuestion();
 }
 
@@ -46,8 +50,28 @@ function endQuestion(point){
 }
 
 function end(){
+    document.getElementById("mainButton").classList.remove("hide");
     for (let i = 0; i < buttons.length; i++){
         buttons[i].remove();
     }
-    document.getElementById("headerText").innerHTML = "You got " + points + " points";
+    document.getElementById("headerText").innerHTML = "Test completed. You got " + points + " points"
+    document.getElementById("questionText").innerHTML = "You are ";
+    if (points < 3){
+        document.getElementById("questionText").innerHTML += "small pick";
+        document.getElementById("image").src = "1pick.jpg";
+    } else {
+        if (points < 6) {
+            document.getElementById("questionText").innerHTML += "medium pick";
+            document.getElementById("image").src = "2pick.jpg";
+        } else {
+            if (points < 9) {
+                document.getElementById("questionText").innerHTML += "hard pick";
+                document.getElementById("image").src = "3pick.jpg";
+            } else {
+                document.getElementById("questionText").innerHTML += "stupid pick";
+                document.getElementById("image").src = "4pick.jpg";
+            }
+        }
+    }
+    document.getElementById("image").classList.add("active");
 }
